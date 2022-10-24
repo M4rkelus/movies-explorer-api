1613f69883fc9d4af6b7d1bc6ff8fd32435f424d
Mark Pirogov <m4rkpirogov@gmail.com>
2022-10-25 01:51:52 +0300

    fix: cors
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

--- utils/configConstants.js
+++ utils/configConstants.js
@@ -9,14 +9,7 @@ const DEV_DATABASE = 'mongodb://localhost:27017/moviesdb';
 const DEV_JWT_SECRET = 'dev-secret';
 
 const CORS_OPTIONS = {
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://movies-explorer-mrk.nomorepartiesxyz.ru',
-    'http://api.movies-explorer-mrk.nomorepartiesxyz.ru',
-    'https://movies-explorer-mrk.nomorepartiesxyz.ru',
-    'https://api.movies-explorer-mrk.nomorepartiesxyz.ru',
-  ],
+  origin: allowedCors,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
